e1affd2421c2aaccbf7c16aaec27a2843846480d
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-06-16 13:46:55 +0200

    feat: remove sw

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import ReactDOM from 'react-dom';
 
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import App from './Components/App';
 
 if (typeof window !== 'undefined') injectTapEventPlugin();
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-registerServiceWorker();
